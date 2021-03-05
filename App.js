2074bf6420cf8d12e197b005e111502c2f5318c1
Danger <joaoperigo>
2021-03-05 18:12:06 -0300

    Primeiro

--- App.js
+++ App.js
@@ -1,15 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      meuTexto: "Um texto inicial",
+      contador:  numberMega()
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.contador}</Text>
+        <Button 
+          title="Incrementar"
+          onPress={ _ => this.setState({contador: numberMega() })}
+        />
+      </View>
+    )
+  }
 }
+
+function numberMega() {
+  let arrayMega = [getRandomInt(60)];
+  while(arrayMega.length < 6) {
+    let number = getRandomInt(60);
+    if(arrayMega.indexOf(number) < 0) { // can't repeat number
+      arrayMega.push(number);   
+    }
+  }
+
+  for(let i=5; i>0; i--) // for decoration
+    arrayMega.splice(i, 0,  ' + ');
+  
+  return arrayMega;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)+1);
+}
+
+// //hook
+// export default function App() {
+//   const [outputText, setOutputText] = useState('Texto inicial');
+//   return (
+//     <View style={styles.container}>
+//       <Text>{outputText}</Text>
+//       <Button 
+//         title="Alterar Texto"
+//         onPress={() => {setOutputText('Novo Texto')}}
+//       />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
